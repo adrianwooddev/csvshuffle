9ba646cf9de2dc9185f0df77fbb782dc3863dcab
Adrian Wood <adrian.wood@derby-college.ac.uk>
2017-11-14 16:42:52 +0000

    Started on implementing file readering, parsing

--- csvshuffle/csvshuffle/Form1.Designer.cs
+++ csvshuffle/csvshuffle/Form1.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbInputFile = new System.Windows.Forms.Label();
+            this.tbInputFile = new System.Windows.Forms.TextBox();
+            this.tbOutputFile = new System.Windows.Forms.TextBox();
+            this.lbOutputFile = new System.Windows.Forms.Label();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbInputFile
+            // 
+            this.lbInputFile.AutoSize = true;
+            this.lbInputFile.Location = new System.Drawing.Point(21, 31);
+            this.lbInputFile.Name = "lbInputFile";
+            this.lbInputFile.Size = new System.Drawing.Size(50, 13);
+            this.lbInputFile.TabIndex = 0;
+            this.lbInputFile.Text = "Input File";
+            // 
+            // tbInputFile
+            // 
+            this.tbInputFile.Location = new System.Drawing.Point(90, 28);
+            this.tbInputFile.Name = "tbInputFile";
+            this.tbInputFile.Size = new System.Drawing.Size(489, 20);
+            this.tbInputFile.TabIndex = 1;
+            this.tbInputFile.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.tbInputFile_MouseDoubleClick);
+            // 
+            // tbOutputFile
+            // 
+            this.tbOutputFile.Location = new System.Drawing.Point(90, 54);
+            this.tbOutputFile.Name = "tbOutputFile";
+            this.tbOutputFile.Size = new System.Drawing.Size(489, 20);
+            this.tbOutputFile.TabIndex = 3;
+            this.tbOutputFile.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.tbOutputFile_MouseDoubleClick);
+            // 
+            // lbOutputFile
+            // 
+            this.lbOutputFile.AutoSize = true;
+            this.lbOutputFile.Location = new System.Drawing.Point(21, 57);
+            this.lbOutputFile.Name = "lbOutputFile";
+            this.lbOutputFile.Size = new System.Drawing.Size(58, 13);
+            this.lbOutputFile.TabIndex = 2;
+            this.lbOutputFile.Text = "Output File";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcess.Location = new System.Drawing.Point(599, 31);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(84, 39);
+            this.btnProcess.TabIndex = 6;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnProcess_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(699, 98);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.tbOutputFile);
+            this.Controls.Add(this.lbOutputFile);
+            this.Controls.Add(this.tbInputFile);
+            this.Controls.Add(this.lbInputFile);
+            this.Name = "Form1";
+            this.Text = "CSV Shuffle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbInputFile;
+        private System.Windows.Forms.TextBox tbInputFile;
+        private System.Windows.Forms.TextBox tbOutputFile;
+        private System.Windows.Forms.Label lbOutputFile;
+        private System.Windows.Forms.Button btnProcess;
     }
 }
 
--- csvshuffle/csvshuffle/Form1.cs
+++ csvshuffle/csvshuffle/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using FileHelpers;
 
 namespace csvshuffle
 {
@@ -16,5 +18,72 @@ namespace csvshuffle
         {
             InitializeComponent();
         }
+
+        private void tbInputFile_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.InitialDirectory = "c:\\";
+            ofd.Filter = "csv files (*.csv)|*.csv|txt files (*.txt)|*.txt|All Files (*.*)|*.*";
+            ofd.FilterIndex = 1;
+            ofd.RestoreDirectory = true;
+
+            DialogResult result = ofd.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                tbInputFile.Text = ofd.FileName;
+            }
+        }
+
+        private void tbOutputFile_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.InitialDirectory = "c:\\";
+            ofd.Filter = "csv files (*.csv)|*.csv|txt files (*.txt)|*.txt|All Files (*.*)|*.*";
+            ofd.FilterIndex = 1;
+            ofd.RestoreDirectory = true;
+
+            DialogResult result = ofd.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                tbOutputFile.Text = ofd.FileName;
+            }
+        }
+
+        private void btnProcess_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(tbInputFile.Text.Length > 1 & tbOutputFile.Text.Length > 1 )
+            {
+                var fh = new FileHelperEngine<record>();
+                var input = fh.ReadFile(tbInputFile.Text);
+
+                foreach (record line in input)
+                {
+                    var tempList = new List<string>();
+                    Console.WriteLine("Thing");
+                    Console.WriteLine(line.ID);
+                    Console.WriteLine(line.quote);
+
+                    tempList[0] = line.ID.ToString();
+                    tempList[1] = line.quote.ToString();
+
+                    string[] outputLine = tempList.ToArray();
+                }
+            }
+        }
     }
+}
+
+[DelimitedRecord(","), IgnoreFirst(1)]
+public class record
+{
+    public int ID;
+    public int number;
+    public string alpha;
+    public string lz;
+    public string quote;
+    public string apo;
 }
